346446adf4af07fcfd6a0bd202c47e364b8cf0b7
Jonas Holbech <jh@jonasholbech.dk>
2022-02-01 21:16:19 +0100

    lets see how much i can break

--- main.js
+++ main.js
@@ -2,16 +2,7 @@ const allDivs = document.querySelectorAll("main>div");
 
 allDivs.forEach((div) => {
   div.style.backgroundColor = `hsl(${Math.random() * 360}, 70%, 70%)`;
-  div.style.transform = `translate(calc(${
+  div.style.transform = `translate(${Math.random() * 100}vw, ${
     Math.random() * 100
-  }vw - 100px), calc(${Math.random() * 100}vh - 100px))`;
-
-  div.addEventListener("mouseenter", () => {
-    div.style.backgroundColor = `hsl(${Math.random() * 360}, 70%, 70%)`;
-    div.style.transform = `translate(calc(${
-      Math.random() * 100
-    }vw - 100px), calc(${Math.random() * 100}vh - 100px)) scale(${
-      Math.random() * 2
-    })`;
-  });
+  }vh)`;
 });
