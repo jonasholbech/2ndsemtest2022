ac62c049740e273566746c787e1b2d5ac6b9a46d
Jonas Holbech <jh@jonasholbech.dk>
2022-02-01 13:47:52 +0100

    animate bubbles on "hover"

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const allDivs = document.querySelectorAll("main>div");
 
 allDivs.forEach((div) => {
   div.style.backgroundColor = `hsl(${Math.random() * 360}, 70%, 70%)`;
-  div.style.transform = `translate(${Math.random() * 100}vw, ${
+  div.style.transform = `translate(calc(${
     Math.random() * 100
-  }vh)`;
+  }vw - 100px), calc(${Math.random() * 100}vh - 100px))`;
+
+  div.addEventListener("mouseenter", () => {
+    div.style.backgroundColor = `hsl(${Math.random() * 360}, 70%, 70%)`;
+    div.style.transform = `translate(calc(${
+      Math.random() * 100
+    }vw - 100px), calc(${Math.random() * 100}vh - 100px))`;
+  });
 });
